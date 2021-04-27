bcfac08a5f3739873b596f8f87ace734383efcb3
Marcelo Finkielman <marcelofinkielman@hotmail.com>
2021-04-27 16:15:42 -0700

    server.js arranged

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-import express from 'express'
+let express = require("express");
+let app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+app.use("/myPage", require("./routes/warehouses"));
+app.use("/createEvent", require("./routes/inventory"));
+app.listen(port, () => console.log(`Express listening on port ${port}`));
